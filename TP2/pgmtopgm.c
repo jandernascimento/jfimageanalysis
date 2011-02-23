709a1b5cc1dfe0b12b02bc6b7d32f883be3205db
jbotnascimento <jbotnascimento@be85e623-b519-c1e4-6e83-4c7242654ca2>
2011-02-23 21:09:05 +0000

    fixing convolution

--- TP2/pgmtopgm.c
+++ TP2/pgmtopgm.c
@@ -95,6 +95,8 @@ gray * stretchingHistogram(char * title,double newRangeMin, double newRangeMax, 
 
 /** Code for filtering:jander **/
 
+int lrows, lcols, lmaxval;
+
 void printkernel(int dim, double *kernel){
  for(int i=0;i<3;i++){
   	for(int j=0;j<3;j++){
@@ -104,7 +106,7 @@ void printkernel(int dim, double *kernel){
   }
 }
 
-void setPixel(char *image,int width, int height, int x,int y, int value){
+void setPixel(double *image,int width, int height, int x,int y, int value){
 
   if(x>width || y>height){
   	printf("Setpixel: Out of range x:%i y:%i",x,y);
@@ -126,7 +128,7 @@ int getPixel(char *image,int width, int height, int x,int y){
 
 }
 
-int findMax(int* array, int len)
+int findMax(double* array, int len)
 {
     int max = array[0];
     for (int i=1;i<len; ++i)
@@ -134,7 +136,7 @@ int findMax(int* array, int len)
             max = array[i];
     return max;
 }
-int findMin(int* array, int len)
+int findMin(double* array, int len)
 {
     int min = array[0];
     for (int i=1;i<len; ++i)
@@ -142,7 +144,7 @@ int findMin(int* array, int len)
             min = array[i];
     return min;
 }
-void minMax(int* oldArr, int oldMin, int oldMax, int newMin, int newMax, int len)
+void minMax(double* oldArr, int oldMin, int oldMax, int newMin, int newMax, int len)
 {
     double tmp;
     for (int i=0;i<len; ++i)
@@ -152,73 +154,7 @@ void minMax(int* oldArr, int oldMin, int oldMax, int newMin, int newMax, int len
     }
 }
 
-int ApplyConvolution(int dim, double* kernel, int width, int height, gray *image){
-       int j;  // row    index of the current image
-       int i;  // column index of the current image
-       int jk; // row    index of the kernel;
-       int ik; // column index of the kernel;
-       int newval[3]; // new colors
-       int size = height*width;
-       //int* tmpImageR = (int*)(malloc(sizeof(int)*size)); // separated by colors
-       int* tmpImageG = (int*)(malloc(sizeof(int)*size)); // separated by colors
-       //int* tmpImageB = (int*)(malloc(sizeof(int)*size)); // separated by colors
-       int kernelCenteri; // index of the central column of the kernel
-       int kernelCenterj; // index of the central row of the kernel
-       double kernelTotalValue;
-       kernelCenteri = dim / 2;
-       kernelCenterj = dim / 2;
-       kernelTotalValue = 0.0;
-       for (j = 0; j < dim; j++)
-         for(i = 0; i < dim; i++)
-           kernelTotalValue += (double)(kernel[j*dim+i]);
-
-       if (kernelTotalValue<=0)
-           kernelTotalValue=1;
-       // convolution computation
-       for (j = 0; j < height; j++) {
-         for (i = 0; i < width; i++) {
-           //newval[0] = 0;
-           newval[1] = 0;
-           //newval[2] = 0;
-           for (jk = 0; jk < dim; jk++) {
-             for (ik = 0; ik < dim; ik++) {
-               int ii = i + ik - kernelCenteri;
-               int jj = j + jk - kernelCenterj;
-               if ((jj >= 0) && (jj <height ) && (ii >= 0) && (ii < width))
-               {
-                   // DO ME
- 		   //newval[0] += getPixelColorIntensity(ImageAbstraction::red,jj,ii) * (double)(kernel[jk*dim+ik]);
-                   //newval[1] += getPixelColorIntensity(ImageAbstraction::green,jj,ii) * (double)(kernel[jk*dim+ik]);
-                   newval[1] += ((double)getPixel(image,width,height,ii,jj)) * 
-					((double)kernel[jk*dim+ik]);
-                   //newval[2] += getPixelColorIntensity(ImageAbstraction::blue,jj,ii) * (double)(kernel[jk*dim+ik]);
-               }
-             }
-           }
-           //newval[0] = newval[0] / kernelTotalValue;
-           newval[1] = newval[1] / kernelTotalValue;
-           //newval[2] = newval[2] / kernelTotalValue;
-
-           //tmpImageR[i*height+j] = newval[0];
-           tmpImageG[j*width+i] = newval[1];
-           //tmpImageB[i*height+j] = newval[2];
-         }
-       }
-
-       //minMax(tmpImageR,findMin(tmpImageR,size),findMax(tmpImageR,size),0,255,size);
-       //minMax(tmpImageG,findMin(tmpImageG,size),findMax(tmpImageG,size),0,255,size);
-       //minMax(tmpImageB,findMin(tmpImageB,size),findMax(tmpImageB,size),0,255,size);
-       for (int i=0; i<height;++i)
-            for (int j=0;j<width;++j){
-                //DO ME
-		//setPixel(i,j,tmpImageR[j*this->height()+i],tmpImageG[j*this->height()+i],tmpImageB[j*this->height()+i]);
-		//setPixel(image,width,height,j,i,getPixel(tmpImageG,width,height,j,i));		
-		setPixel(image,width,height,j,i,tmpImageG[i*width+j]);
-		}
-       return 1;
-}
-
-void printimage(gray* image,int cols, int rows, int maxval){
+void printimage(double* image,int cols, int rows, int maxval){
 
 printf("P2\n");
     printf("%d %d \n", cols, rows);
@@ -226,14 +162,14 @@ printf("P2\n");
 
     for(int i=0; i < rows; i++)
       for(int j=0; j < cols ; j++)
-	printf("%i ",image[i * cols + j] );
+	printf("%i ",(int)image[i * cols + j] );
 
 }
 
-gray* readimage(int argc, char* argv[])
+double* readimage(int argc, char* argv[])
     {
     FILE* ifp;
-    gray* graymap;
+    double* imagemap;
     int ich1, ich2;
     int pgmraw;
     int j,i ;
@@ -266,20 +202,20 @@ gray* readimage(int argc, char* argv[])
       else pgmraw = 0;
 
     /* Lecture des dimensions */
-    cols = pm_getint( ifp );
-    rows = pm_getint( ifp );
-    maxval = pm_getint( ifp );
+    lcols = pm_getint( ifp );
+    lrows = pm_getint( ifp );
+    lmaxval = pm_getint( ifp );
 
     /* Allocation memoire  */
-    graymap = (gray *) malloc(cols * rows * sizeof(gray));
+    imagemap = (double *) malloc(lcols * lrows * sizeof(double));
 
     
     if(pgmraw){
     /* Convertendo para Plaintext */
     /* Lecture */
-	    for(i=0; i < rows; i++)
-	      for(j=0; j < cols ; j++)
-		graymap[i * cols + j] = pm_getint(ifp);
+	    for(i=0; i < lrows; i++)
+	      for(j=0; j < lcols ; j++)
+		imagemap[i * lcols + j] = pm_getint(ifp);
 
     }else { 
         printf("Invalid file type");
@@ -287,54 +223,107 @@ gray* readimage(int argc, char* argv[])
     }
       /* fermeture */
       fclose(ifp);
-      return graymap;
+      return imagemap;
 }
 
+double* ApplyConvolution(int dim, double* kernel, double* image, int imageH, int imageW){
+        //minMaxDouble(kernel,-1,1,0,1,9);
+    /*
+        for (int i=0;i<dim;++i)
+            for (int j=0;j<dim;++j)
+                qDebug("%f KERNEL", (double)(kernel[i*dim+j]));
+     */
+       int j;  // row    index of the current image
+       int i;  // column index of the current image
+       int jk; // row    index of the kernel;
+       int ik; // column index of the kernel;
+	int ii, jj;
+       double newval; // new colors
+       int size = imageH*imageW;
+       double* tmpImage = (double*)(malloc(sizeof(double)*size));
+       int kernelCenteri; // index of the central column of the kernel
+       int kernelCenterj; // index of the central row of the kernel
+       double kernelTotalValue;
+       kernelCenteri = dim / 2;
+       kernelCenterj = dim / 2;
+       kernelTotalValue = 0.0;
+       for (j = 0; j < dim; j++)
+         for(i = 0; i < dim; i++)
+           kernelTotalValue += (double)(kernel[j*dim+i]);
+       if (kernelTotalValue<=0)
+           kernelTotalValue=1;
+       // convolution computation
+       for (j = 0; j < imageH; j++) {
+         for (i = 0; i < imageW; i++) {
+           newval = 0;
+           for (jk = 0; jk < dim; jk++) {
+             for (ik = 0; ik < dim; ik++) {
+               ii = i + ik - kernelCenteri;
+               jj = j + jk - kernelCenterj;
+               if ((jj >= 0) && (jj <imageH ) && (ii >= 0) && (ii < imageW))
+               {
+                   newval += image[jj*imageW+ii]*(double)(kernel[jk*dim+ik]);
+               }
+             }
+           }
+           newval = newval / kernelTotalValue;
+
+           tmpImage[j*imageW+i] = newval;
+
+         }
+       }
+       minMax(tmpImage,findMin(tmpImage,size),findMax(tmpImage,size),0,255,size);
+       return tmpImage;
+}
+gray* image_strectched;
+
+double* binomialfilter()
+{
+	double *kernel = (double*)malloc(sizeof(double)*9);
+
+	kernel[0*3+0]=1;
+	kernel[0*3+1]=2;
+	kernel[0*3+2]=1;
+
+	kernel[1*3+0]=2;
+	kernel[1*3+1]=4;
+	kernel[1*3+2]=2;
+
+	kernel[2*3+0]=1;
+	kernel[2*3+1]=2;
+	kernel[2*3+2]=1;
+	return kernel;
+}
 
 /** Code for filtering:end **/
 
 int main(int argc, char* argv[]){
-gray* image;
-gray* image_strectched;
 
-double *kernel;
+	double* image;
+	double *kernel;
 
-kernel=(double *)malloc(sizeof(double)*9);
 
-kernel[0*3+0]=1;
-kernel[0*3+1]=2;
-kernel[0*3+2]=1;
-
-kernel[1*3+0]=2;
-kernel[1*3+1]=4;
-kernel[1*3+2]=2;
-
-kernel[2*3+0]=1;
-kernel[2*3+1]=2;
-kernel[2*3+2]=1;
-
-image=readimage(argc,argv);
-
-//ApplyConvolution(3, kernel, cols, rows, image);
-
-//printimage(image,cols,rows,maxval);
+	kernel=binomialfilter();
+	image=readimage(argc,argv);
+	image=ApplyConvolution(3, kernel, image, lrows, lcols);
+	printimage(image,lcols,lrows,lmaxval);
  
-//****** RAQUEL *************//
-//generate histogram
-generateHistogramValues("\nHistogram's values\n\n",image);
+	//****** RAQUEL *******//
+	//generate histogram
+	//generateHistogramValues("\nHistogram's values\n\n",image);
+	//displayImageFile(image);
+	//generate histogram
+	//generateHistogramValues(rows,cols,ifp);
 
-//write the image on the screen
-//displayImageFile(image);
+	//histogram stretching
+	//image_strectched=stretchingHistogram("\nStretched Histogram's values\n\n", 0,255,image);
 
-//histogram stretching
-image_strectched=stretchingHistogram("\nStretched Histogram's values\n\n", 0,255,image);
+	//write the image on the screen
+	//displayImageFile(image_strectched);
 
-//write the image on the screen
-displayImageFile(image_strectched);
-
-free(image);
-free(image_strectched);
-return 0;
+	//free(image);
+	//free(image_strectched);
+	return 0;
 
 }
 
