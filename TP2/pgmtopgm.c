13d4bcd162872031e1e31b7431c63570b860257c
jbotnascimento <jbotnascimento@be85e623-b519-c1e4-6e83-4c7242654ca2>
2011-02-23 09:58:04 +0000

    fixing get/set

--- TP2/pgmtopgm.c
+++ TP2/pgmtopgm.c
@@ -93,13 +93,15 @@ int ApplyConvolution(int dim, double* kernel, int width, int height, char *image
                    // DO ME
  		   //newval[0] += getPixelColorIntensity(ImageAbstraction::red,jj,ii) * (double)(kernel[jk*dim+ik]);
                    //newval[1] += getPixelColorIntensity(ImageAbstraction::green,jj,ii) * (double)(kernel[jk*dim+ik]);
+                   newval[1] += getPixel(image,width,height,ii,jj) * 
+					(double)(kernel[jk*dim+ik]);
                    //newval[2] += getPixelColorIntensity(ImageAbstraction::blue,jj,ii) * (double)(kernel[jk*dim+ik]);
                }
              }
            }
-           newval[0] = newval[0] / kernelTotalValue;
+           //newval[0] = newval[0] / kernelTotalValue;
            newval[1] = newval[1] / kernelTotalValue;
-           newval[2] = newval[2] / kernelTotalValue;
+           //newval[2] = newval[2] / kernelTotalValue;
 
            //tmpImageR[i*height+j] = newval[0];
            tmpImageG[i*height+j] = newval[1];
@@ -118,9 +120,102 @@ int ApplyConvolution(int dim, double* kernel, int width, int height, char *image
        return 1;
 }
 
+char* readimage(int argc, char* argv[])
+    {
+    FILE* ifp;
+    gray* graymap;
+    int ich1, ich2, rows, cols, maxval, pgmraw,j,i ;
+
+
+
+    /* Test des arguments */
+    if ( argc != 2 ){
+      printf("\nUsage : %s fichier \n\n", argv[0]);
+      exit(0);
+    }
+
+    /* Ouverture */
+    ifp = fopen(argv[1],"r");
+    if (ifp == NULL) {
+      printf("erreur d'ouverture du fichier %s\n", argv[1]);
+      exit(1);
+    }
+
+    /* Lecture du Magic number */
+    ich1 = getc( ifp );
+    if ( ich1 == EOF )
+        pm_erreur( "EOF / erreur de lecture / nombre magique" );
+    ich2 = getc( ifp );
+    if ( ich2 == EOF )
+        pm_erreur( "EOF / erreur de lecture / nombre magique" );
+    if(ich2 != '2' && ich2 != '5')
+      pm_erreur(" mauvais type de fichier ");
+    else
+      if(ich2 == '2')
+	pgmraw = 0;
+      else pgmraw = 1;
+
+    /* Lecture des dimensions */
+    cols = pm_getint( ifp );
+    rows = pm_getint( ifp );
+    maxval = pm_getint( ifp );
+
+    /* Allocation memoire  */
+    graymap = (gray *) malloc(cols * rows * sizeof(gray));
+
+    
+    if(pgmraw){
+    /* Convertendo para Plaintext */
+    /* Lecture */
+	    for(i=0; i < rows; i++)
+	      for(j=0; j < cols ; j++)
+		graymap[i * cols + j] = pm_getrawbyte(ifp);
+
+      	    printf("P2\n");
+
+	    printf("%d %d \n", cols, rows);
+	    printf("%d\n",maxval);
+	   
+	    for(i=0; i < rows; i++)
+	      for(j=0; j < cols ; j++)
+		printf("%u ",graymap[i * cols + j] );
+
+    }
+/*
+    else {
+    for(i=0; i < rows; i++)
+      for(j=0; j < cols ; j++)
+	graymap[i * cols + j] = pm_getint(ifp);
+
+     printf("P5\n");
+
+	printf("%d %d \n", cols, rows);
+	    printf("%d\n",maxval);
+	   
+	    for(i=0; i < rows; i++)
+	      for(j=0; j < cols ; j++)
+		printf("%i",graymap[i * cols + j] );
+
+    }
+*/
+    
+
+
+      /* fermeture */
+      fclose(ifp);
+      return 0;
+}
+
 
 /** Code for filtering:end **/
 
 int main(int argc, char* argv[]){
+
+char image[16];
+char kernel[9];
+
+
+ApplyConvolution(&kernel,3,4,4,image);
+
 
 }
