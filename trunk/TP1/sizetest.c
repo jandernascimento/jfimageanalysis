c250c0165b34219a8d894a75a427e679e6797261
jbotnascimento <jbotnascimento@be85e623-b519-c1e4-6e83-4c7242654ca2>
2011-02-23 08:55:36 +0000

    adding base code

---
+++ trunk/TP1/sizetest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(void){
+
+char c;
+int i;
+unsigned int ui;
+float f;
+
+printf("char:%i\nint:%i\nunsigned int:%i\nfloat:%i\n",
+sizeof(unsigned char),sizeof(i),sizeof(ui),sizeof(f));
+
+}
