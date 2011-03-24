8e8e0218602a786996f9973fb694516b6f3ceeb8
raraujo.oliveira <raraujo.oliveira@be85e623-b519-c1e4-6e83-4c7242654ca2>
2011-03-24 23:00:53 +0000

    parcial revisao

--- trunk/TP3/doc/compile_report.sh
+++ trunk/TP3/doc/compile_report.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm ./report.log report.pdf report.toc report.dvi report.blg report.bbl report.aux;
-pdflatex report.tex && pdflatex ./report.tex && acroread ./report.pdf
+pdflatex report.tex && pdflatex ./report.tex && acroread -openInNewInstance ./report.pdf
