bf3de32424783d6148c784fe87ca0161d74b5655
jbotnascimento <jbotnascimento@be85e623-b519-c1e4-6e83-4c7242654ca2>
2011-03-31 17:12:33 +0000

    fixing issue with picture

--- TP4/showregion.c
+++ TP4/showregion.c
@@ -4,12 +4,13 @@
 
 
 // HEADER
-void printimage(double* image,int cols, int rows, int maxval);
-double* readimage(char* filepath);
+void printimage(gray* image,int cols, int rows, int maxval);
+gray* readimage(char* filepath);
 int rows=0,cols=0,maxval=0;
+#define DPC 3 //data per cell information
 // BODY
 
-void printimage(double* image,int rows,int cols,int maxval){
+void printimage(gray* image,int rows,int cols,int maxval){
 
     printf("P3\n");
     printf("%d %d \n", cols, rows);
@@ -17,20 +18,20 @@ void printimage(double* image,int rows,int cols,int maxval){
 
     for(int i=0; i < rows; i++)
       for(int j=0; j < cols ; j++){
-	printf("%i\n",(int)image[i * cols + j+0] );
-	printf("%i\n",(int)image[i * cols + j+1] );
-	printf("%i\n",(int)image[i * cols + j+2] );
+	printf("%u ",image[DPC*i * cols + DPC*j+0] );
+	printf("%u ",image[DPC*i * cols + DPC*j+1] );
+	printf("%u \n",image[DPC*i * cols + DPC*j+2] );
       }
 
 }
 
-double* readimage(char* filepath)
+gray* readimage(char* filepath)
     {
     FILE* ifp;
-    double* imagemap;
+    gray* imagemap;
     int ich1, ich2;
     int validppm;
-    int j,i ;
+    //int j,i ;
     
     ifp = fopen(filepath,"r");
     if (ifp == NULL) {
@@ -48,9 +49,10 @@ double* readimage(char* filepath)
     //if(ich2 != '2' && ich2 != '5')
     //    pm_erreur(" mauvais type de fichier ");
     //else
-      if(ich2 == '3')
-	validppm = 1;
-      else validppm = 0;
+      if(ich2 != '3'){
+	fprintf(stderr,"Invalid file type");
+        exit(1);
+      }
 
     /* Lecture des dimensions */
     cols = pm_getint( ifp );
@@ -58,23 +60,18 @@ double* readimage(char* filepath)
     maxval = pm_getint( ifp );
 
     /* Allocation memoire  */
-    imagemap = (double *) malloc(3 * cols * rows * sizeof(double));
+    imagemap = (gray *) malloc(3 * cols * rows * sizeof(gray));
 
-    
-    if(validppm){
     /* Lecture */
-	    for(i=0; i < rows; i++){
-	      for(j=0; j < cols ; j++){
-		imagemap[i * cols + j + 0] = pm_getint(ifp);
-		imagemap[i * cols + j + 1] = pm_getint(ifp);
-		imagemap[i * cols + j + 2] = pm_getint(ifp);
-		}
-	    }
+    for(int i=0; i < rows; i++){
+      for(int j=0; j < cols ; j++){
+	fprintf(stderr,"Indo..");
+	imagemap[DPC* i * cols + DPC*j + 0] = pm_getint(ifp);
+	imagemap[DPC* i * cols + DPC*j + 1] = pm_getint(ifp);
+	imagemap[DPC* i * cols + DPC*j + 2] = pm_getint(ifp);
+	}
+     }
 
-    }else { 
-        fprintf(stderr,"Invalid file type");
-        exit(1);
-    }
       /* fermeture */
       fclose(ifp);
       return imagemap;
@@ -83,70 +80,7 @@ double* readimage(char* filepath)
 
 int main(int argc, char* argv[]){
 
-	double *im=readimage("image/clownplaintext.ppm");
+	gray *graymap=readimage("image/clownplaintext.ppm");
 
-	printimage(im,rows,cols,maxval);
-
+	printimage(graymap,rows,cols,maxval);
 }
-
-/*
-int main(int argc, char* argv[]){
-    FILE* ifp;
-    gray* graymap;
-    int ich1, ich2, rows, cols, maxval, pgmraw,j,i ;
-
-    if ( argc != 2 ){
-      printf("\nUsage : %s fichier \n\n", argv[0]);
-      exit(0);
-    }
-
-    ifp = fopen(argv[1],"r");
-    if (ifp == NULL) {
-      printf("erreur d'ouverture du fichier %s\n", argv[1]);
-      exit(1);
-    }
-
-    ich1 = getc( ifp );
-    if ( ich1 == EOF )
-        pm_erreur( "EOF / erreur de lecture / nombre magique" );
-    ich2 = getc( ifp );
-    if ( ich2 == EOF )
-        pm_erreur( "EOF / erreur de lecture / nombre magique" );
-    if(ich2 != '3'){// && ich2 != '5')
-      pm_erreur(" mauvais type de fichier ");
-      pgmraw = 0;
-    }else
-		pgmraw = 1;
-    cols = pm_getint( ifp );
-    rows = pm_getint( ifp );
-    maxval = pm_getint( ifp );
-
-    graymap = (gray *) malloc(3 * cols * rows * sizeof(gray));
-    
-    if(pgmraw){
-	    for(i=0; i < rows; i++)
-	      for(j=0; j < cols ; j++){
-		graymap[i * cols + j + 0] = pm_getint(ifp);
-		graymap[i * cols + j + 1] = pm_getint(ifp);
-		graymap[i * cols + j + 2] = pm_getint(ifp);
-		}
-
-      	    printf("P3\n");
-
-	    printf("%d %d \n", cols, rows);
-	    printf("%d\n",maxval);
-	   
-	    for(i=0; i < rows; i++)
-	      for(j=0; j < cols ; j++)
-		printf("%i ",(graymap[i * cols + j+0]+graymap[i * cols + j+1]+graymap[i * cols + j+2])/3 );
-		//printf("%u ",graymap[i * cols + j] );
-
-    }else {
-	    printf("ERROR!\n");
-    }
-
-      fclose(ifp);
-      return 0;
-}
-
-*/
