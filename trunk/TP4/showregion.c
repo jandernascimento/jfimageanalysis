cc6efac8a80d8537c9bc7093d3ec8ef7d46e56dd
jbotnascimento <jbotnascimento@be85e623-b519-c1e4-6e83-4c7242654ca2>
2011-03-31 19:07:38 +0000

    making structures

--- trunk/TP4/showregion.c
+++ trunk/TP4/showregion.c
@@ -1,38 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "Util.h"
-
-
-// HEADER
-void printimage(gray* image,int cols, int rows, int maxval);
-gray* readimage(char* filepath);
-int rows=0,cols=0,maxval=0;
+#include "showregion.h"
 #define DPC 3 //data per cell information
-// BODY
 
-void printimage(gray* image,int rows,int cols,int maxval){
+void printimage(pimage_type image){
 
     printf("P3\n");
-    printf("%d %d \n", cols, rows);
-    printf("%d\n",maxval);
+    printf("%d %d \n", image->cols, image->rows);
+    printf("%d\n",image->maxval);
 
-    for(int i=0; i < rows; i++)
-      for(int j=0; j < cols ; j++){
-	printf("%u ",image[DPC*i * cols + DPC*j+0] );
-	printf("%u ",image[DPC*i * cols + DPC*j+1] );
-	printf("%u \n",image[DPC*i * cols + DPC*j+2] );
+    for(int i=0; i < image->rows; i++)
+      for(int j=0; j < image->cols ; j++){
+	printf("%u ",image->stream[DPC*i * image->cols + DPC*j+0] );
+	printf("%u ",image->stream[DPC*i * image->cols + DPC*j+1] );
+	printf("%u \n",image->stream[DPC*i * image->cols + DPC*j+2] );
       }
 
 }
 
-gray* readimage(char* filepath)
+pimage_type readimage(char* filepath)
     {
     FILE* ifp;
     gray* imagemap;
     int ich1, ich2;
-    int validppm;
-    //int j,i ;
-    
+    pimage_type image=(pimage_type)malloc(sizeof(image_type));  
+ 
     ifp = fopen(filepath,"r");
     if (ifp == NULL) {
       fprintf(stderr,"Error openning the file, check if you specified -i. Path: %s\n", filepath);
@@ -55,31 +47,33 @@ gray* readimage(char* filepath)
       }
 
     /* Lecture des dimensions */
-    cols = pm_getint( ifp );
-    rows = pm_getint( ifp );
-    maxval = pm_getint( ifp );
+    image->cols = pm_getint( ifp );
+    image->rows = pm_getint( ifp );
+    image->maxval = pm_getint( ifp );
 
     /* Allocation memoire  */
-    imagemap = (gray *) malloc(3 * cols * rows * sizeof(gray));
+    imagemap = (gray *) malloc(3 * image->cols * image->rows * sizeof(gray));
 
     /* Lecture */
-    for(int i=0; i < rows; i++){
-      for(int j=0; j < cols ; j++){
-	imagemap[DPC* i * cols + DPC*j + 0] = pm_getint(ifp);
-	imagemap[DPC* i * cols + DPC*j + 1] = pm_getint(ifp);
-	imagemap[DPC* i * cols + DPC*j + 2] = pm_getint(ifp);
+    for(int i=0; i < image->rows; i++){
+      for(int j=0; j < image->cols ; j++){
+	imagemap[DPC* i * image->cols + DPC*j + 0] = pm_getint(ifp);
+	imagemap[DPC* i * image->cols + DPC*j + 1] = pm_getint(ifp);
+	imagemap[DPC* i * image->cols + DPC*j + 2] = pm_getint(ifp);
 	}
      }
 
+     image->stream=imagemap;
+
       /* fermeture */
       fclose(ifp);
-      return imagemap;
+      return image; //imagemap;
 }
 
 
 int main(int argc, char* argv[]){
 
-	gray *graymap=readimage("image/clownplaintext.ppm");
+	pimage_type graymap=readimage("image/clownplaintext.ppm");
 
-	printimage(graymap,rows,cols,maxval);
+	printimage(graymap);
 }
