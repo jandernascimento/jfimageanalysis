29966fafb464759772ba7f9cc5bdbc957e16c32a
jbotnascimento <jbotnascimento@be85e623-b519-c1e4-6e83-4c7242654ca2>
2011-04-13 09:18:14 +0000

    base project

---
+++ trunk/TP5/Util.c
@@ -0,0 +1,93 @@
+/* Utilitaires de lecture pour les fichiers PBM, PGM, PPM
+**
+**
+** E.B. 12/98
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Util.h"
+
+char pm_getc(FILE* file)
+    {
+    int ich;
+    char ch;
+
+    ich = getc( file );
+    if ( ich == EOF )
+        pm_erreur("EOF / erreur de lecture" );
+    ch = (char) ich;
+
+    if ( ch == '#' )
+        {
+        do
+            {
+            ich = getc( file );
+            if ( ich == EOF )
+                pm_erreur("EOF / erreur de lecture" );
+            ch = (char) ich;
+            }
+        while ( ch != '\n' && ch != '\r' );
+        }
+
+    return ch;
+    }
+
+
+bit pm_getbit(FILE* file)
+    {
+    char ch;
+
+    do
+        {
+        ch = pm_getc( file );
+        }
+    while ( ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' );
+
+    if ( ch != '0' && ch != '1' )
+        pm_erreur("erreur de lecture : != [0|1]" );
+
+    return ( ch == '1' ) ? 1 : 0;
+    }
+
+
+unsigned char pm_getrawbyte(FILE* file)
+    {
+    int iby;
+
+    iby = getc( file );
+    if ( iby == EOF )
+        pm_erreur("EOF / erreur de lecture" );
+    return (unsigned char) iby;
+    }
+
+int pm_getint( FILE* file)
+    {
+    char ch;
+    int i;
+
+    do
+        {
+        ch = pm_getc( file );
+        }
+    while ( ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' );
+
+    if ( ch < '0' || ch > '9' )
+        pm_erreur( "erreur de lecture : != [0-9]" );
+
+    i = 0;
+    do
+        {
+        i = i * 10 + ch - '0';
+        ch = pm_getc( file );
+        }
+    while ( ch >= '0' && ch <= '9' );
+
+    return i;
+    }
+
+void pm_erreur(char *texte)
+{
+  fprintf(stderr, "\n%s \n\n", texte);
+  exit(1);
+}
---
+++ trunk/TP5/tp5.h
@@ -0,0 +1,29 @@
+#include "Util.h"
+#define DPC 1   //data per cell information
+#define RED 0   //index of the red in DPC
+#define GREEN 1 //index of the green in DPC
+#define BLUE 2  //index of the blue in DPC
+#define K 3     //index of the group k in DPC
+
+// STRUCTURE
+typedef struct tpixel {
+
+  int r,g,b;
+  int x,y;
+  int k; //number of the group that the pixel belongs to
+} pixel_type; //, *ppixel_type;
+
+
+typedef struct timage {
+
+  int cols,rows;
+  int maxval;
+  gray* stream;
+  char* path;
+} image_type, *pimage_type;
+
+// METHODS
+pimage_type readimage(char* filepath);
+void printimage(pimage_type image);
+void set_pixel(pimage_type image,int x, int y, pixel_type pixel );
+pixel_type *get_pixel(pimage_type image,int x, int y);
